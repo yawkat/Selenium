060fbe60f720e2c524bfc6e5e22ba82a7051d0d1
Jonas Konrad <me@yawk.at>
2013-06-30 20:14:34 +0200

    load TrueZIP drivers

--- src/main/java/at/yawk/selenium/fs/Zip.java
+++ src/main/java/at/yawk/selenium/fs/Zip.java
@@ -3,9 +3,18 @@ package at.yawk.selenium.fs;
 import java.io.File;
 import java.nio.file.Path;
 
+import de.schlichtherle.truezip.file.TArchiveDetector;
+import de.schlichtherle.truezip.file.TConfig;
+import de.schlichtherle.truezip.fs.archive.zip.ZipDriverService;
+import de.schlichtherle.truezip.fs.nio.file.FileDriverService;
 import de.schlichtherle.truezip.nio.file.TPath;
 
 public class Zip {
+    static {
+        TConfig.get().setArchiveDetector(new TArchiveDetector(TConfig.get().getArchiveDetector(), new ZipDriverService().get()));
+        TConfig.get().setArchiveDetector(new TArchiveDetector(TConfig.get().getArchiveDetector(), new FileDriverService().get()));
+    }
+    
     private Zip() {}
     
     public static Path toPath(File file) {
